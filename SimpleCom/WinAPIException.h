7e3df9b49a710ee258eaf2722d6277a5966a279b
Nathan Brown <nathan@nkbrown.us>
2021-04-05 15:29:46 -0700

    Make WinAPIException safer.

--- SimpleCom/WinAPIException.h
+++ SimpleCom/WinAPIException.h
@@ -33,19 +33,19 @@ private:
 	LPTSTR _error_text;
 
 public:
-	WinAPIException(DWORD error_code) : WinAPIException(error_code, _T("SimpleCom")) {};
+	explicit WinAPIException(DWORD error_code) : WinAPIException(error_code, _T("SimpleCom")) {}
 	WinAPIException(DWORD error_code, LPCTSTR error_caption);
 	virtual ~WinAPIException();
 
-	inline DWORD GetErrorCode() noexcept {
+	inline DWORD GetErrorCode() const noexcept {
 		return _error_code;
 	}
 
-	inline LPCTSTR GetErrorCaption() noexcept {
+	inline LPCTSTR GetErrorCaption() const noexcept {
 		return _error_caption;
 	}
 
-	inline LPTSTR GetErrorText() noexcept {
+	inline LPCTSTR GetErrorText() const noexcept {
 		return _error_text;
 	}
 };
