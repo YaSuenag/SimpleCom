3cb3c1674c95bb67314d07dead0beae915402b0e
Yasumasa Suenaga <yasuenag@gmail.com>
2021-05-30 11:40:28 +0900

    Add ZIP distribution archiver for release build

---
+++ zip-packaging.ps1
@@ -0,0 +1,17 @@
+﻿param(
+    [string]$version,
+    [string]$solutiondir
+)
+
+$distname = "SimpleCom-$version"
+$destdir = "$solutiondir\dist\$distname"
+
+Remove-Item -Recurse -Force $destdir*
+New-Item $destdir -itemType Directory
+Copy-Item $solutiondir\x64\Release\SimpleCom.exe $destdir
+Copy-Item $solutiondir\README.md $destdir
+Copy-Item $solutiondir\fragments.json $destdir
+
+[Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
+echo "$solutiondir\dist\$distname.zip"
+[System.IO.Compression.ZipFile]::CreateFromDirectory($destdir, "$solutiondir\dist\$distname.zip", [System.IO.Compression.CompressionLevel]::Optimal, $true)
