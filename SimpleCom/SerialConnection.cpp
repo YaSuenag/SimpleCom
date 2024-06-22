YaSuenag/SimpleCom

commit 939f11d03852a2c2f5818afa18e7dfbf933592c6

Don't propagate escape sequence of F1

SimpleCom/SerialConnection.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023, Yasumasa Suenaga
+ * Copyright (C) 2023, 2024, Yasumasa Suenaga
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -78,6 +78,9 @@ bool SimpleCom::SerialConnection::ProcessKeyEvents(const KEY_EVENT_RECORD keyeve
 			SetEvent(hTermEvent);
 			return true;
 		}
+
+		// Return immediately without issueing escape sequence of F1
+		return false;
 	}
 
 	if (keyevent.bKeyDown && (keyevent.uChar.AsciiChar != '\0')) {
@@ -195,6 +198,18 @@ bool SimpleCom::SerialConnection::StdInRedirector(const HANDLE hSerial, const HA
 
 				for (DWORD idx = 0; idx < n_read; idx++) {
 					if (inputs[idx].EventType == KEY_EVENT) {
+						// Skip escape sequence of F1 (0x1B 0x4F 0x50)
+						//
+						// Input sequence on Windows (ENABLE_VIRTUAL_TERMINAL_INPUT):
+						//   https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#numpad--function-keys
+						if (idx + 2 < n_read &&
+							(inputs[idx].Event.KeyEvent.wRepeatCount == 1 && inputs[idx].Event.KeyEvent.uChar.AsciiChar == '\x1b' /* ESC */) &&
+							(inputs[idx + 1].Event.KeyEvent.wRepeatCount == 1 && inputs[idx + 1].Event.KeyEvent.uChar.AsciiChar == 'O') &&
+							(inputs[idx + 2].Event.KeyEvent.wRepeatCount == 1 && inputs[idx + 2].Event.KeyEvent.uChar.AsciiChar == 'P')) {
+							idx += 2;
+							continue;
+						}
+
 						if (ProcessKeyEvents(inputs[idx].Event.KeyEvent, writer, hTermEvent)) {
 							return true;
 						}

commit 926ce855366ea708dcb6d6787104a4f2697e95a5

F1 key does not work on modern Windows 11
Relates to #44

This issue is caused by:
https://github.com/YaSuenag/SimpleCom/commit/939f11d03852a2c2f5818afa18e7dfbf933592c6

This change worked at the timing of the commit (Jan 6 2024) at least,
but it does not work at June 22 2024 on Windows 11 23H2 (22631.3737).
So I guess this bug is closely related to Windows updates between
Jan 2024 and June 2024, but I could not figure out the KB which relates
to this.

SimpleCom/SerialConnection.cpp
@@ -77,23 +77,29 @@ void SimpleCom::SerialConnection::InitSerialPort(const HANDLE hSerial) {
 }
 
 /*
- * Write key code in KEY_EVENT_RECORD to send buffer.
- * If F1 key is found, all of send_data would be flushed and would set true to terminated flag.
- * Return true if F1 key is pushed (active close).
+ * Ask user whether terminate current serial session via dialog box.
+ * Return true if session should be closed (active close).
  */
-bool SimpleCom::SerialConnection::ProcessKeyEvents(const KEY_EVENT_RECORD keyevent, SerialPortWriter& writer, const HANDLE hTermEvent) {
+bool SimpleCom::SerialConnection::ShouldTerminate(SerialPortWriter& writer, const HANDLE hTermEvent) {
+	// Write all keys in the buffer before F1
+	writer.WriteAsync();
+
+	if (MessageBox(_parent_hwnd, _T("Do you want to leave from this serial session?"), _T("SimpleCom"), MB_YESNO | MB_ICONQUESTION) == IDYES) {
+		SetEvent(hTermEvent);
+		return true;
+	}
+
+	return false;
+}
+
+/*
+ * Write key code in KEY_EVENT_RECORD to send buffer.
+ */
+void SimpleCom::SerialConnection::ProcessKeyEvents(const KEY_EVENT_RECORD keyevent, SerialPortWriter& writer) {
 
 	if (keyevent.wVirtualKeyCode == VK_F1) {
-		// Write all keys in the buffer before F1
-		writer.WriteAsync();
-
-		if (MessageBox(_parent_hwnd, _T("Do you want to leave from this serial session?"), _T("SimpleCom"), MB_YESNO | MB_ICONQUESTION) == IDYES) {
-			SetEvent(hTermEvent);
-			return true;
-		}
-
-		// Return immediately without issueing escape sequence of F1
-		return false;
+		// F1 key should not be propagated to peripheral.
+		return;
 	}
 
 	if (keyevent.bKeyDown && (keyevent.uChar.AsciiChar != '\0')) {
@@ -104,8 +110,6 @@ bool SimpleCom::SerialConnection::ProcessKeyEvents(const KEY_EVENT_RECORD keyeve
 			}
 		}
 	}
-
-	return false;
 }
 
 static void StdOutRedirectorLoopInner(const HANDLE hSerial, OVERLAPPED *overlapped, const HANDLE hStdOut, SimpleCom::LogWriter* logwriter) {
@@ -226,12 +230,15 @@ bool SimpleCom::SerialConnection::StdInRedirector(const HANDLE hSerial, const HA
 							(inputs[idx + 1].Event.KeyEvent.wRepeatCount == 1 && inputs[idx + 1].Event.KeyEvent.uChar.AsciiChar == 'O') &&
 							(inputs[idx + 2].Event.KeyEvent.wRepeatCount == 1 && inputs[idx + 2].Event.KeyEvent.uChar.AsciiChar == 'P')) {
 							idx += 2;
-							continue;
+							if (ShouldTerminate(writer, hTermEvent)) {
+								return true;
+							}
+							else{
+								continue;
+							}
 						}
 
-						if (ProcessKeyEvents(inputs[idx].Event.KeyEvent, writer, hTermEvent)) {
-							return true;
-						}
+						ProcessKeyEvents(inputs[idx].Event.KeyEvent, writer);
 					}
 					else if ((inputs[idx].EventType == WINDOW_BUFFER_SIZE_EVENT) && _useTTYResizer) {
 						char buf[RINGBUF_SZ];

SimpleCom/SerialConnection.h
@@ -38,7 +38,8 @@ namespace SimpleCom {
 		bool _enableStdinLogging;
 
 		void InitSerialPort(const HANDLE hSerial);
-		bool ProcessKeyEvents(const KEY_EVENT_RECORD keyevent, SerialPortWriter& writer, const HANDLE hTermEvent);
+		bool ShouldTerminate(SerialPortWriter& writer, const HANDLE hTermEvent);
+		void ProcessKeyEvents(const KEY_EVENT_RECORD keyevent, SerialPortWriter& writer);
 		bool StdInRedirector(const HANDLE hSerial, const HANDLE hTermEvent);
 
 	public: