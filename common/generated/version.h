f866ea06adfcefcdd2508fc083bd39901661bbfe
Yasumasa Suenaga <yasuenag@gmail.com>
2024-08-11 15:50:19 +0900

    Up to v1.2.4

--- common/generated/version.h
+++ common/generated/version.h
@@ -4,8 +4,8 @@
 
 #define MAJOR 1
 #define MINOR 2
-#define PATCH 3
+#define PATCH 4
 #define BUILD 0
 
-#define SEMVER "1.2.3"
+#define SEMVER "1.2.4"
 #define LEGAL_COPYRIGHT "Copyright (C) 2019, 2024, Yasumasa Suenaga"
