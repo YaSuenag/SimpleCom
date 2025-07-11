905f02dda567da63849e393a5b4249c42c7fc519
Yasumasa Suenaga <yasuenag@gmail.com>
2025-07-11 22:20:11 +0900

    Small fix

--- SimpleCom/BatchRedirector.cpp
+++ SimpleCom/BatchRedirector.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2025, Yasumasa Suenaga
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
 #include "stdafx.h"
 #include "BatchRedirector.h"
 
@@ -19,7 +37,7 @@ DWORD WINAPI BatchStdInRedirector(_In_ LPVOID lpParameter) {
 	HANDLE hSerial = reinterpret_cast<HANDLE>(lpParameter);
 	char buf[buf_sz];
 	DWORD nBytesRead;
-	while(ReadFile(hStdIn, buf, sizeof(buf), &nBytesRead, NULL) && nBytesRead > 0) {
+	while(ReadFile(hStdIn, buf, sizeof(buf), &nBytesRead, NULL)) {
 		DWORD nBytesWritten;
 		DWORD nBytesRemain = nBytesRead;
 		while (nBytesRemain > 0) {
